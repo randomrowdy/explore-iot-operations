61839b15c8ecc04c32190d6dcf9a8741186d8fbf
randomrowdy <115378034+randomrowdy@users.noreply.github.com>
2025-02-16 20:12:24 -0800

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -5,5 +5,10 @@ set -o nounset
 set -o pipefail
 
 echo "Starting On Create Command"
-
+# Create k3d cluster and forwarded ports
+k3d cluster delete
+k3d cluster create \
+-i rancher/k3s:v1.32.1-rc2-k3s1 \
+-p '1883:1883@loadbalancer' \
+-p '8883:8883@loadbalancer'
 echo "Ending On Create Command"
